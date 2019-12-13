8c7b63ae057c6f0679529e45a38fd8a1d7867166
Splend1d <a24998667@gmail.com>
2019-12-13 17:50:01 +0800

    added database

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,15 @@
 			$banner = $('#banner');
 
 		var $height = $('#header').height();
+<<<<<<< Updated upstream
 
+=======
+        //force reload -- (ADDED)
+		    // $(document).ready(function () {
+		    //     $('html').animate({ scrollTop: 0 }, 1);
+		    //     $body.animate({ scrollTop: 0 }, 1);
+		    // });
+>>>>>>> Stashed changes
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
 
