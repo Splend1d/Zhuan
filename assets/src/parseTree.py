edf5acbbe87152ac2a5d528c2c1b692df2c1f248
Splend1d <a24998667@gmail.com>
2019-12-18 14:34:20 +0800

    update parts!

--- assets/src/parseTree.py
+++ assets/src/parseTree.py
@@ -1,13 +1,29 @@
 import json
 import pandas as pd
-
-with open('../db/dbtree.json', 'r') as f:
-    parsed = json.load(f)
-parsed = parsed[0:2]
 with open('../db/dbtree.json', 'r') as f:
     parsed = json.load(f)
 components = {}
-for i,p in enumerate(parsed):
+
+df = pd.read_csv('../db/dball.csv')
+fontid_map_font = pd.Series(df.楷書字型.values,index=df.字體編號).to_dict() # for human eye evaluation
+p = parsed[0]
+zhuan_map_parent = p
+for k,vs in p.items():
+	for v in vs:
+		try:
+			components[v].append(k)
+		except:
+			components[v] = [k]
+basic = []
+maxv = 0
+for k,v in components.items():
+	if len(v) > maxv:
+		maxv = len(v)
+		maxk = k
+	if len(v) == 1:
+		basic.append(fontid_map_font[k])
+
+for i,p in enumerate(parsed[1:]):
 	print(i)
 	try:
 		print(p['27.53E3'])
@@ -15,16 +31,20 @@ for i,p in enumerate(parsed):
 		print("no")
 	for k,vs in p.items():
 		for v in vs:
-			try:
-				components[v].append(k)
-			except:
-				components[v] = [k]
-
+			if v in basic:
+				try:
+					components[v].append(k)
+				except:
+					s() #this should never happen
+					components[v] = [k]
+zhuan_map_child = components
 print("c",components['27.F6A2'])
-maxv = 0
-basic = []
-df = pd.read_csv('../db/dball.csv')
-fontid_map_font = pd.Series(df.楷書字型.values,index=df.字體編號).to_dict()
+
+
+# a basic character is of rank 1
+# two basic characters is rank 2
+# 
+
 for k,v in components.items():
 	if len(v) > maxv:
 		maxv = len(v)
@@ -33,3 +53,14 @@ for k,v in components.items():
 		basic.append(fontid_map_font[k])
 print(basic)
 print(maxk,maxv)
+
+with open("../db/dblearntree.js","w",encoding = "utf8") as f:
+	f.write("var zhuan_map_child =")
+	f.write(str(zhuan_map_child))
+	f.write(';\n')
+	f.write("var zhuan_map_parent =")
+	f.write(str(zhuan_map_parent))
+	f.write(';')
+	print("writing")
+print(zhuan_map_child)
+#print(zhuan_map_parent)
