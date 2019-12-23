cc97ae906f120cd2e865c0800d2f8156a47a67dc
Splend1d <a24998667@gmail.com>
2019-12-23 20:01:19 +0800

    restructure data

--- assets/src/archived/getimg.py
+++ assets/src/archived/getimg.py
@@ -2,50 +2,22 @@ import requests
 from requests_html import HTMLSession
 
 from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
-import json
-import pickle as pkl
+import csv
 import pandas as pd
 
 import sys
 import os
 
-imgsown = set(os.listdir('../db/img'))
-print(imgsown)
-
-with open('../db/main.pkl','r') as f:
-  db = json.load(f)
-
-imgsneedkai = []
-imgsneedzhuan = []
-for v in db.values():
-  imgsneedkai += v['fonts'][0] 
-  imgsneedzhuan += v['fonts'][1] # NEED　TO CHANGE
-imgsnotownzhuan = []
-imgsnotownkai = []
-for img in imgsneedkai + imgsneedzhuan:
-  if img + '.png' not in imgsown:
-    if int(img.split('.')[0]) < 27:
-      imgsnotownkai.append(img)
-    else:
-      imgsnotownzhuan.append(img)
-print("needed:",len(imgsneedzhuan),"zhuan fonts")
-print("needed:",len(imgsneedkai),"kai fonts")
-
-font_infos = []
-print("unowned:",len(imgsnotownzhuan),"zhuan fonts")
-print("unowned:",len(imgsnotownkai),"kai fonts")
-s()
-for n,id_ in enumerate(imgsnotownzhuan):
+df = pd.read_csv('../db/db.csv')
+imgids = df["字體編號"]
+imgmap = [["字體編號","楷書字體編號","楷書字型"]]
+for n,id_ in enumerate(imgids):
     print("retrieving img of: ",id_," No. ",n)
     session = HTMLSession()
     r = session.get('http://xiaoxue.iis.sinica.edu.tw/char?fontcode='+id_)
     imgs_html = r.html.find('img')
-    temp_info = [i.attrs["alt"][1:-1] for i in imgs_html if "alt" in i.attrs ]
     font_html = r.html.find(' td[style="font-size: 48px"]')
     font = font_html[0].text
-    temp_info.append(font)
-    print(temp_info)
-    font_infos.append(temp_info)
     c = 0
     tempmap = []
     for i in imgs_html:
@@ -72,9 +44,12 @@ for n,id_ in enumerate(imgsnotownzhuan):
        
        r = requests.get(img_url_l, allow_redirects=True)
        if "alt" in i.attrs:
-         open('../db/img/'+i.attrs['alt'][1:-1]+'.png', 'wb').write(r.content)
-         #print(i.attrs)
-       
-
-with open('../db/font.pkl', 'wb',encoding = "utf8") as f:
-    json.dump(font_infos,f)
+         open('./db/img2/'+i.attrs['alt'][1:-1]+'.png', 'wb').write(r.content)
+         print(i.attrs)
+       tempmap.append(i.attrs['alt'][1:-1])
+    tempmap.append(font)
+    imgmap.append(tempmap) 
+ 
+with open('../db/dbimg.csv', 'w', newline='',encoding = "utf8") as f:
+    writer = csv.writer(f)
+    writer.writerows(imgmap)
